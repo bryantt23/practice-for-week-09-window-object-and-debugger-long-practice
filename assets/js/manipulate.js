f3c1541b3d3246e4f99d1623db01175c5c3d71f4
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-11 01:03:12 -0700

    change text

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,20 @@
-function changeTitle() {
-    // Change the title of the page to "(Your name)'s Portfolio"
-
-    // Your code here
+export function changeTitle() {
+  // Change the title of the page to "(Your name)'s Portfolio"
+  // Your code here
+  document.title = "Bryant's Portfolio";
 }
 
-function changeHeader() {
-    // Change the name in the h1 of the page to your name
-
-    // Your code here
+export function changeHeader() {
+  // Change the name in the h1 of the page to your name
+  // Your code here
+  document.querySelector('h1').textContent = 'I am Bry';
 }
 
-function changeAboutMe() {
-    /* Update the first paragraph in the About Me section with a small
+export function changeAboutMe() {
+  /* Update the first paragraph in the About Me section with a small
      passage about yourself */
-
-    // Your code here
+  // Your code here
+  const aboutMe = document.querySelector('.section');
+  aboutMe.childNodes[3].textContent = 'This is about Bry';
+  debugger;
 }
